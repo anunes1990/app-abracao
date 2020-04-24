14a4981f293301479ba64eda1e71ca15bf2f2384
allisson.dedigo <allisson.dedigo@giergs.org.br>
2020-04-23 23:04:50 -0300

    Configuração da libary Picasso

--- app/src/main/java/com/allisson/appabracao/adapter/OngsAdpater.kt
+++ app/src/main/java/com/allisson/appabracao/adapter/OngsAdpater.kt
@@ -6,16 +6,19 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.allisson.appabracao.DetalheOng
 import com.allisson.appabracao.R
 import com.allisson.appabracao.models.Ong
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item.view.*
 
 class OngsAdpater(
     private val myDataset: List<Ong>,
     private val context: Context) :
     RecyclerView.Adapter<OngsAdpater.MyViewHolder>() {
+    lateinit var imageView: ImageView
     class MyViewHolder(val viewRoot: View) : RecyclerView.ViewHolder(viewRoot)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OngsAdpater.MyViewHolder {
@@ -25,14 +28,19 @@ class OngsAdpater(
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.viewRoot.txt_detail.text = "${myDataset[position].nome} - ${myDataset[position].email}"
-
+        holder.viewRoot.txt_nome.text = myDataset[position].nome
+        imageView = holder.viewRoot.iv_ong;
+        loadImageFromUrl(myDataset[position].foto)
         holder.viewRoot.setOnClickListener {
             val intent = Intent(context, DetalheOng::class.java)
             intent.putExtra("ong", myDataset[position])
             context.startActivity(intent)
         }
+    }
 
+    private fun loadImageFromUrl(url: String) {
+        Picasso.get().load(url).resize(300, 300)
+            .centerCrop().into(imageView)
     }
 
     override fun getItemCount() = myDataset.size;
--- app/src/main/java/com/allisson/appabracao/ui/listaongs/ListaOngsViewModel.kt
+++ app/src/main/java/com/allisson/appabracao/ui/listaongs/ListaOngsViewModel.kt
@@ -14,7 +14,7 @@ class ListaOngsViewModel : ViewModel() {
             "Ong TESTE 1",
             "Rua Correa de Mello, 1045",
             25,
-            "foto1",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSxdIgc7wBcx2YTCGi9EaYn6EqZtLN3-ZBn_oakGP5Qzr7m4A57&usqp=CAU",
             "(51) 3365-6812",
             "ong1@ong1.com.br",
             "ong1.com.br"
@@ -24,7 +24,27 @@ class ListaOngsViewModel : ViewModel() {
             "Ong TESTE 2",
             "Rua Correa de Mello, 1045",
             325,
-            "foto2",
+            "https://i1.wp.com/www.blogueirosdasaude.org.br/wp-content/uploads/2017/12/simbolo.jpg?fit=458%2C218&ssl=1",
+            "(51) 3365-7079",
+            "ong2@ong2.com.br",
+            "ong2.com.br"
+        )
+
+        val ong3 = Ong(
+            "Ong TESTE 3",
+            "Rua Correa de Mello, 1045",
+            25,
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSxdIgc7wBcx2YTCGi9EaYn6EqZtLN3-ZBn_oakGP5Qzr7m4A57&usqp=CAU",
+            "(51) 3365-6812",
+            "ong1@ong1.com.br",
+            "ong1.com.br"
+        )
+
+        val ong4 = Ong(
+            "Ong TESTE 4",
+            "Rua Correa de Mello, 1045",
+            325,
+            "https://i1.wp.com/www.blogueirosdasaude.org.br/wp-content/uploads/2017/12/simbolo.jpg?fit=458%2C218&ssl=1",
             "(51) 3365-7079",
             "ong2@ong2.com.br",
             "ong2.com.br"
@@ -33,6 +53,8 @@ class ListaOngsViewModel : ViewModel() {
         val tempOngs: MutableList<Ong> = ArrayList();
         tempOngs.add(ong1)
         tempOngs.add(ong2)
+        tempOngs.add(ong3)
+        tempOngs.add(ong4)
 
         ongs.value = tempOngs;
     }
